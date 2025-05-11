dc32998db96342ea35c38d6a0cfd3eb698f5b894
Darkhan <dmukhamedyarov.engineer@gmail.com>
2025-05-11 19:51:08 +0500

    Added IP check and DNS is valid

--- health_check.py
+++ health_check.py
@@ -1,5 +1,5 @@
 from health_check.cli import parse_cli_args
-from health_check.checker import check_http
+from health_check.checker import check_http, check_dns, is_public_ip, check_ports
 from health_check.logger import get_logger
 from health_check.diagnostics import run_basic_diagnostics, run_full_diagnostics
 
@@ -7,17 +7,37 @@ def main():
     args = parse_cli_args()
     domain = args.url.split('/')[2]
     logger = get_logger(args.log_file)
-    result = check_http(args.url, args.keyword, args.timeout, logger)
-    if not result:
-        
-        # Логируем ошибку, что keyword не найден или статус не 200
-        # (можно передавать причину из check_http, если нужно конкретнее)
-        logger.error('[HTTP_CHECK] - Health check failed for url %s with keyword "%s"', args.url, args.keyword)
-        if args.full_diagnostics:
-            logger.info("[CONFIG] - Full diagnostics mode enabled")
-            run_full_diagnostics(domain, args.url, logger, args.timeout)
-        else:
-            run_basic_diagnostics(domain, args.url, logger, args.timeout)
+    resolvable, ips = check_dns(domain)
+    if not resolvable:
+        logger.error("[DNS_CHECK] - DNS resolution failed for %s", args.url)
+    else:
+        logger.info("[DNS_CHECK] - DNS resolution successful, IPs: %s", ', '.join(ips))
+        for ip in ips:
+            if is_public_ip(ip):
+                logger.info("[IP_CHECK] - %s is public IP", ip)
+                
+                ports_to_check = [80, 443]
+                results = check_ports(ip, ports_to_check)
+
+                for port in ports_to_check:
+                    if results[port]:
+                        logger.info("[PORT_ACCESS] - Access to %s:%s is available", ip, port)
+                    else:
+                        logger.error("[PORT_ACCESS] - Access to %s:%s is not available", ip, port)
+            else:
+                logger.warning("[IP_CHECK] - %s private or reserve IP", ip)
+
+        result = check_http(args.url, args.keyword, args.timeout, logger)
+        if not result:
+            
+            # Логируем ошибку, что keyword не найден или статус не 200
+            # (можно передавать причину из check_http, если нужно конкретнее)
+            logger.error('[HTTP_CHECK] - Health check failed for url %s with keyword "%s"', args.url, args.keyword)
+            if args.full_diagnostics:
+                logger.info("[CONFIG] - Full diagnostics mode enabled")
+                run_full_diagnostics(domain, args.url, logger, args.timeout)
+            else:
+                run_basic_diagnostics(domain, args.url, logger, args.timeout)
 
 if __name__ == "__main__":
     main()
--- health_check/checker.py
+++ health_check/checker.py
@@ -1,7 +1,45 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import dns.resolver
+import ipaddress
+import socket
+
 from datetime import datetime
+
+def check_dns(domain):
+    try:
+        answers = dns.resolver.resolve(domain, 'A')
+        ip_list = [rdata.address for rdata in answers]
+        return True, ip_list
+    except dns.resolver.NXDOMAIN:
+        return False, []
+    except dns.resolver.NoAnswer:
+        return False, []
+    except dns.resolver.Timeout:
+        return False, []
+
+def is_public_ip(ip):
+    ip_obj = ipaddress.ip_address(ip)
+    return not (ip_obj.is_private or ip_obj.is_loopback or ip_obj.is_reserved)
+
+# def check_port(ip, port, timeout=3):
+#     try:
+#         with socket.create_connection((ip, port), timeout=timeout):
+#             return True
+#     except (socket.timeout, socket.error):
+#         return False
+    
+def check_ports(ip, ports, timeout=3):
+    results = {}
+    for port in ports:
+        try:
+            with socket.create_connection((ip, port), timeout=timeout):
+                results[port] = True
+        except (socket.timeout, socket.error):
+            results[port] = False
+    return results
+
 
 def check_http(url, keyword, timeout, logger):
     try:
